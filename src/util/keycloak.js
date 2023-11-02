8174b388cf8b332aa2a7cb516586a532d12c1970
CCS-2022 <128265192+CCS-2022@users.noreply.github.com>
2023-11-01 22:53:00 -0400

    Update keycloak.js

--- src/util/keycloak.js
+++ src/util/keycloak.js
@@ -1,6 +1,6 @@
 import Keycloak from "keycloak-js";
 const keycloak = new Keycloak({
-  url: "http://192.168.1.236:8080/",
+  url: "https://192.168.1.236:8443/",
   realm: "SecretSantaDev",
   clientId: "React-auth",
 });
